b6e9c5ce7a22d4ec58a56f0d3595fd20e1f4a9f7
Gabriel Borela <gabrielborela@MacBook-Air-de-Gabriel.local>
2026-03-28 19:58:52 -0300

    mudou firebase

--- BotFuneraria/core/firebase.py
+++ BotFuneraria/core/firebase.py
@@ -3,36 +3,29 @@ import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-db = None
+# 🔥 inicializa automático
 
+if not firebase_admin._apps:
 
-def iniciar_firebase():
-    global db
+    cred_json = os.getenv("FIREBASE_CREDENTIALS")
 
-    if not firebase_admin._apps:
+    if cred_json:
+        cred_dict = json.loads(cred_json)
+        cred = credentials.Certificate(cred_dict)
 
-        # 🔹 PRIORIDADE 1 — ENV (Render)
-        cred_json = os.getenv("FIREBASE_CREDENTIALS")
+    else:
+        caminho_arquivo = "firebase_credentials.json"
 
-        if cred_json:
-            cred_dict = json.loads(cred_json)
-            cred = credentials.Certificate(cred_dict)
+        if not os.path.exists(caminho_arquivo):
+            raise Exception("Arquivo firebase_credentials.json não encontrado")
 
-        else:
-            # 🔹 PRIORIDADE 2 — ARQUIVO LOCAL
-            caminho_arquivo = "firebase_credentials.json"
+        cred = credentials.Certificate(caminho_arquivo)
 
-            if not os.path.exists(caminho_arquivo):
-                raise Exception("Arquivo firebase_credentials.json não encontrado")
+    firebase_admin.initialize_app(cred)
 
-            cred = credentials.Certificate(caminho_arquivo)
-
-        firebase_admin.initialize_app(cred)
-
-    # 🔥 ISSO TEM QUE FICAR FORA DO IF
-    db = firestore.client()
+# 🔥 db sempre pronto
+db = firestore.client()
 
 
 def salvar_pedido(dados):
-    iniciar_firebase()
     db.collection("pedidos").add(dados)
